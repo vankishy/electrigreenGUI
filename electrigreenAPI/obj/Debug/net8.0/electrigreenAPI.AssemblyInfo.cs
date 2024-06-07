acc902e0175e9a9ec19866c5ff4a768ec8de237b
unknown <kusumarizky650@gmail.com>
2024-06-07 17:44:18 +0700

    iseng buka ajah

--- electrigreenAPI/obj/Debug/net8.0/electrigreenAPI.AssemblyInfo.cs
+++ electrigreenAPI/obj/Debug/net8.0/electrigreenAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("electrigreenAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+360383b339b93da28f5d7166e932e1b641d348e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ac8612ff7c701e8066208ade47e73bc5a79848")]
 [assembly: System.Reflection.AssemblyProductAttribute("electrigreenAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("electrigreenAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
